1ca3729b4efdd9f4f03f1f98360851cbddec18a1
Vikram Bhamre <vikrambhamre@berkeley.edu>
2025-09-02 21:56:37 -0700

    layers.py first commit

--- nn/init.py
+++ nn/init.py
@@ -2,11 +2,14 @@ import jax.numpy as jnp
 
 def kaiming_init(key, shape, dtype):
     std = jnp.sqrt(2 / shape[0])
-    return std * jnp.normal(key, shape, dtype)
+    return std * jnp.random.normal(key, shape, dtype)
 
-def constant_int(key, shape, dtype, c=0.0):
-    return c * jnp.ones(shape, dtype=dtype)
+def constant_init(key, shape, dtype, c=0.0):
+    return c * jnp.ones(shape, dtype)
 
 def xavier_init(key, shape, dtype):
     abs_bound = jnp.sqrt(6 / (shape[0] + shape[1]))
-    return jnp.uniform(key, shape, dtype, minval=-abs_bound, maxval=abs_bound)
+    return jnp.random.uniform(key, shape, dtype, minval=-abs_bound, maxval=abs_bound)
+
+def zero_init(key, shape, dtype):
+    return jnp.zeros(shape, dtype)
--- nn/layers.py
+++ nn/layers.py
@@ -1,20 +1,22 @@
-from typing import Callable, Optional, Tuple, Any
+from typing import Callable, Optional, Tuple, Any, Dict
 import jax
 import jax.numpy as jnp
 from jax import random, lax
-from nn.init import kaiming_init, xavier_init, constant_init
+from nn.init import kaiming_init, xavier_init, constant_init, zero_init
 PyTree = Any
 
 def dense_init(
         key: jax.Array,
         in_dim: int,
-        out_dim:int,
+        out_dim: int,
         *,
         w_init=kaiming_init, # (key, shape, dtype) -> jax.Array
-        b_init: bool = True,
+        b_init=zero_init,
         dtype=jnp.float32,
 ) -> Dict[str, jax.Array]:
-
+    w_shape = (out_dim, in_dim)
+    b_shape = (out_dim,)
+    return {'W': w_init(key, w_shape, dtype), 'B': b_init(key, b_shape, dtype)}
     
 
 def dense_apply(
@@ -22,3 +24,18 @@ def dense_apply(
     x: jax.Array,
 ) -> jax.Array:
     
+    y = jnp.matmul(x, jnp.transpose(params['W'])) + params['B']
+    return y
+
+def dropout_apply(
+        x: jax.Array,
+        *,
+        rate: float,
+        key,
+        training: bool = True
+) -> jax.Array:
+    zeroed_matrix = jnp.choice(key, [0, 1], shape=x.shape, p=[rate, 1-rate])
+    if training:
+        x = x * zeroed_matrix
+    return x
+
